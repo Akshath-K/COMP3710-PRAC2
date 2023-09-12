40b1b116ab40d1534c024c6433e9a48393c52001
Akshath-K <110160158+Akshath-K@users.noreply.github.com>
2023-09-12 21:31:38 +1000

    visualization of g's progression for noise reduction

--- part3.py
+++ part3.py
@@ -279,4 +279,21 @@ if __name__ == '__main__':
                 img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
             iters += 1
+
+    plt.figure(figsize=(10,5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(G_losses,label="G")
+    plt.plot(D_losses,label="D")
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
     
+    fig = plt.figure(figsize=(8,8))
+    plt.axis("off")
+    ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+    ani.save('animation.gif')
+
+    HTML(ani.to_jshtml())
+        
