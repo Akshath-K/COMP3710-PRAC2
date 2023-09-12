2562854c0c3134eccdae7d108044efd266d1cfa6
Akshath-K <110160158+Akshath-K@users.noreply.github.com>
2023-09-12 21:33:40 +1000

    plotting real vs fake images side by side for comparrision

--- part3.py
+++ part3.py
@@ -298,4 +298,18 @@ if __name__ == '__main__':
     HTML(ani.to_jshtml())
     
     # Grab a batch of real images from the dataloader
-    real_batch = next(iter(dataloader))    
+    real_batch = next(iter(dataloader))
+    
+    # Plot the real images
+    plt.figure(figsize=(15,15))
+    plt.subplot(1,2,1)
+    plt.axis("off")
+    plt.title("Real Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+    # Plot the fake images from the last epoch
+    plt.subplot(1,2,2)
+    plt.axis("off")
+    plt.title("Fake Images")
+    plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+    plt.show()    
